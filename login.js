d013cffcac60c31f0dbd2806c4eaaea03bfb8a2a
AhamadShaikh <shaikhahamadrasul786@gmail.com>
2022-10-19 10:30:59 +0530

    login password and username

---
+++ login.js
@@ -0,0 +1,17 @@
+let res_user = Ahamad12@gmail.com
+let res_pass = 123456
+
+
+let new_user = Ahamad12@gmail.com
+let new_pass = 123456
+
+if(let res_user = new_user)
+{
+  if(let res_pass = new_pass)
+    {
+       console.log("login successfull");
+    }
+else{
+       console.log("invalid credentials"); 
+ }
+}
